902247766f426eddfe8294bb48cfe33d9eb4d5ba
armgnarda <93938720+armgnarda@users.noreply.github.com>
2024-04-28 16:36:05 +0300

    Add files via upload

---
+++ Karatsuba/src/Karatsuba.java
@@ -0,0 +1,38 @@
+import java.math.BigInteger;
+
+public class Karatsuba {
+
+	public static void main(String[] args) {
+		BigInteger x = new BigInteger("12345678901234567890");
+        BigInteger y = new BigInteger("98765432109876543210");
+
+        BigInteger result = karatsubaMultiply(x, y);
+        System.out.println("Sonuç: " + result);
+
+	}
+	public static BigInteger karatsubaMultiply(BigInteger x, BigInteger y) {
+        int n = Math.max(x.bitLength(), y.bitLength());
+
+        // Karatsuba algoritmasýnýn temel durumu
+        if (n <= 2000) {
+            return x.multiply(y);
+        }
+
+        // Karatsuba için n'in yarýsý
+        n = (n / 2) + (n % 2);
+
+        // x ve y'nin parçalara bölünmesi
+        BigInteger a = x.shiftRight(n);
+        BigInteger b = x.subtract(a.shiftLeft(n));
+        BigInteger c = y.shiftRight(n);
+        BigInteger d = y.subtract(c.shiftLeft(n));
+
+        // Üç Karatsuba çarpýmý
+        BigInteger ac = karatsubaMultiply(a, c);
+        BigInteger bd = karatsubaMultiply(b, d);
+        BigInteger adbc = karatsubaMultiply(a.add(b), c.add(d)).subtract(ac).subtract(bd);
+
+        // Sonuçlarýn birleþtirilmesi
+        return ac.shiftLeft(2 * n).add(adbc.shiftLeft(n)).add(bd);
+    }
+}
